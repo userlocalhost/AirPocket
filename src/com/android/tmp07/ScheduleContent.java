6bc6a2b34bce779c20c01a57ffda8ae5682b1bfd
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2011-04-27 00:16:07 +0900

    correct a problem of deleting and moving overlapped document

--- src/com/android/tmp07/ScheduleContent.java
+++ src/com/android/tmp07/ScheduleContent.java
@@ -52,8 +52,6 @@ public class ScheduleContent
 			ScheduleContent doc = (ScheduleContent) overlappedDocs.get(i);
 
 			if(doc != null) {
-				Log.d(TAG, String.format("[removeObj] overlappedDocument.subject:%s[%s] (index:%d/%d) => %s[%s]",doc.getSubject(), doc.getId(), doc.getIndex(), this.index, this.subject, this.id.toString()));
-
 				doc.setDepth(doc.getDepth() - 1);
 				if(doc.getIndex() > this.index) {
 					doc.setIndex(doc.getIndex() - 1);
@@ -72,8 +70,6 @@ public class ScheduleContent
 
 	public void addOverlappedId(String id) {
 		ScheduleContent doc = getFromId(id);
-
-		Log.d(TAG, String.format("[addOverlap] %s(%s) <= %s", this.subject, this.id.toString(), id));
 
 		this.overlappedDocumentId.add(id);
 	}
